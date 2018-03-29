96bc2cce782af9d016e1a7acae3dc2fd8af92fed
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2018-03-29 11:43:25 +0200

    now using z3 4.6 for ubuntu 16
    released in dec 2017

--- build_z3.sh
+++ build_z3.sh
@@ -21,10 +21,10 @@ if [ ! -f z3.zip ] ; then
 #    curl $DLURL > z3.zip
 
 
-	# using z3 4.5, available on GitHub
-	export DLURL=https://github.com/Z3Prover/z3/releases/download/z3-4.5.0/z3-4.5.0-x64-ubuntu-14.04.zip
+	# using z3 4.6, available on GitHub
+	export DLURL=https://github.com/Z3Prover/z3/releases/download/z3-4.6.0/z3-4.6.0-x64-ubuntu-16.04.zip	
 	wget --progress=dot:mega $DLURL
-	mv z3-4.5.0-x64-ubuntu-14.04.zip z3.zip
+	mv z3-4.6.0-x64-ubuntu-16.04.zip z3.zip
 fi
 
 if [ ! -d z3 ] ; then 
