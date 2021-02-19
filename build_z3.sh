d3599607eeb5e0edddaa4869fb4704cf29ab8c1a
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2021-02-19 13:08:27 +0100

    Use 4.8.10 release of Z3, from Jan 2021.

--- build_z3.sh
+++ build_z3.sh
@@ -21,10 +21,10 @@ if [ ! -f z3.zip ] ; then
 #    curl $DLURL > z3.zip
 
 
-	# using z3 4.8.6, available on GitHub
-	export DLURL=https://github.com/Z3Prover/z3/releases/download/z3-4.8.6/z3-4.8.6-x64-ubuntu-16.04.zip	
+	# using z3 4.8.10, available on GitHub   
+	export DLURL=https://github.com/Z3Prover/z3/releases/download/z3-4.8.10/z3-4.8.10-x64-ubuntu-18.04.zip	
 	wget --progress=dot:mega $DLURL
-	mv z3-4.8.6-x64-ubuntu-16.04.zip z3.zip
+	mv z3-4.8.10-x64-ubuntu-18.04.zip z3.zip
 fi
 
 if [ ! -d z3 ] ; then 
